ba036a9031d951bee0247a400d68c021d9d204c1
Scott Lahteine <github@thinkyhead.com>
2020-05-26 00:19:45 -0500

    Notes about startup sequence

--- extension.js
+++ extension.js
@@ -4,6 +4,19 @@
  *
  * NOTES: For 'command failed' check declarations!
  *        Be sure to escape backslashes in "new Regex()"
+ *
+ * TODO: Standardize startup sequence. Currently this extension intializes before the
+ *       action bar icon is selected to reveal empty list-views with button icons,
+ *       so the Welcome View can be given the correct state for the open workspace.
+ *
+ *       Opening a new folder or a new workspace actually reloads the whole window and
+ *       starts the extension over from the top. So, there is no need to monitor the
+ *       state of the currently open workspace if there's simply no folder open at all.
+ *       The state of the Welcome View will be set correctly on the next startup.
+ *
+ *       That said, if a folder is open in the workspace, it may be watched to see if
+ *       the content changes so that it becomes a valid Marlin installation, as might
+ *       often occur when using Git externally.
  */
 
 'use strict';
